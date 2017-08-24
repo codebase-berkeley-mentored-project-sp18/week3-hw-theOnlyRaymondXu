17f99cb9baac3f03d6b530d597b1a759027cc79c
Andrew Chan <andrewkchan@berkeley.edu>
2017-08-23 22:45:17 -0700

    post details test database dummy data

--- bloggo/posts/tests.py
+++ bloggo/posts/tests.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.test import TestCase, Client
 from .views import about, post_details
+from .models import Post
 
 # Create your tests here.
 class AboutPage(TestCase):
@@ -28,6 +29,22 @@ class AboutPage(TestCase):
 
 class PostDetailsPage(TestCase):
     def setUp(self):
+        Post.objects.create(title="Worst Day Ever",
+                            body="So I was walking outside today and some guy comes up to with a clipboard wearing a "
+                                 "freshly ironed pair of salmon colored chinos and he's like Hey do you care about the "
+                                 "environment. And I'm like yo of course I care about the environment who do you think I "
+                                 "am some kind of self serving schmuck of course I care about the world I live in and how "
+                                 "we as humans affect it and stuff. So yeah. Then hes like well thats great news Im part "
+                                 "of an organization called CalPIRG and you should totally join it if you dont youre an "
+                                 "earth hating piece of crap except he didnt actually say that but you could totally see "
+                                 "that was like the vibe he was giving off. So then Im like ok Ill join your stupid "
+                                 "organization and filled out his form n stuff n then hes like well sykes buddy now "
+                                 "youre gonna pay $10 a semester to my club and im gonna spend it on big bouncy "
+                                 "castles and liquor for all my friends and I was like oh my god you conniving insidious "
+                                 "nefarious poopheads gimme back my money but then he was like too late you already signed "
+                                 "the form and thats the worst day of my life!")
+        Post.objects.create(title="Important Questions",
+                            body="What job did Chandler Bing from friends have?")
         self.c = Client()
         self.r = self.c.get("/posts/details/1")
         self.r2 = self.c.get("/posts/details/2")
